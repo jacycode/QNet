45ed7cdce9cdbb7396bef405f7be9ec32a9ae213
jacycode <liuqing1020liuqing@126.com>
2016-06-05 16:03:10 +0800

    0.2.0

---
+++ UIImageView+QData.h
@@ -0,0 +1,15 @@
+//
+//  UIImageView+QData.h
+//  Test_QNet
+//
+//  Created by 刘清 on 16/6/5.
+//  Copyright © 2016年 LQ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIImageView (QData)
+
+- (void)setImageWithURL:(NSString *)imageURL placeholder:(NSString *)imageName;
+
+@end
---
+++ UIImageView+QData.m
@@ -0,0 +1,27 @@
+//
+//  UIImageView+QData.m
+//  Test_QNet
+//
+//  Created by 刘清 on 16/6/5.
+//  Copyright © 2016年 LQ. All rights reserved.
+//
+
+#import "UIImageView+QData.h"
+
+@implementation UIImageView (QData)
+
+- (void)setImageWithURL:(NSString *)imageURL placeholder:(NSString *)imageName
+{
+    self.image = [UIImage imageNamed:imageName];
+    dispatch_async(dispatch_get_global_queue(0, 0), ^{
+       
+        UIImage *image = [UIImage imageWithData:[NSData dataWithContentsOfURL:[NSURL URLWithString:imageURL]]];
+        
+        dispatch_async(dispatch_get_main_queue(), ^{
+            self.image = image;
+        });
+        
+    });
+}
+
+@end
